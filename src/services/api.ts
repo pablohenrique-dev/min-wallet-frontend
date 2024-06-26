fe78f338a52705082a6230eda1a00088e22ca4fa
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-06-26 10:55:55 -0300

    chore: add api layer and interceptors

---
+++ src/services/api.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:3333",
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+    "Access-Control-Allow-Origin": "*",
+  },
+  withCredentials: true,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const token = localStorage.getItem("token");
+
+    if (error.response.status === 401 && !originalRequest._retry && token) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await axios.patch<{ token: string }>(
+          "http://localhost:3333/token/refresh",
+          null,
+          {
+            withCredentials: true,
+          },
+        );
+
+        const { token } = response.data;
+
+        localStorage.setItem("token", token);
+
+        originalRequest.headers.Authorization = `Bearer ${token}`;
+        return axios(originalRequest);
+      } catch (error) {
+        localStorage.removeItem("token");
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export const apiRoutes = {
+  register: "/register",
+  login: "/sessions",
+  profile: "/me",
+} as const;
