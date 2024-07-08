51ce836658017865b454d40de0523ca3bd717d02
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-07-08 16:44:38 -0300

    feat: add auth layout

---
+++ src/components/layouts/auth.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+export function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <section className="grid h-screen grid-cols-1 md:grid-cols-[640px_auto]">
+      <div className="mx-6 my-32 flex animate-fade-left flex-col items-center justify-center sm:mx-0">
+        {children}
+      </div>
+      <div className="hidden w-full animate-fade-in bg-[url('bg-image-black-pattern.jpg')] bg-cover md:block"></div>
+    </section>
+  );
+}
