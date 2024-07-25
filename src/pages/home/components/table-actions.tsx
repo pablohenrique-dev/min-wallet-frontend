0a99d24a24a59cc1505b68511b1c74527991a53b
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-07-25 10:29:59 -0300

    feat: add select component

--- src/pages/home/components/table-actions.tsx
+++ src/pages/home/components/table-actions.tsx
@@ -6,7 +6,7 @@ export function TableActions() {
   return (
     <div className="mb-6 flex flex-col-reverse items-center justify-between gap-6 sm:flex-row">
       <SearchBar />
-      <div className="flex w-full items-center justify-end gap-4">
+      <div className="flex w-full flex-col items-center justify-end gap-4 sm:flex-row">
         <SelectPeriod />
         <CreateTransactionDialog />
       </div>
